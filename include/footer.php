8a96ae9aa40ba0b2cb86e2ecd2c26b9a9569cfe2
selvaprakash1 <156052592+selvaprakash1@users.noreply.github.com>
2024-07-20 13:45:43 +0530

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,9 @@
+      <!-- footer part start -->
+      <footer class="py-3 my-4">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+          <li class="nav-item"><a href="index.php" class="nav-link px-2 text-muted"><b>Home</b></a></li>
+          <li class="nav-item"><a href="customer.php" class="nav-link px-2 text-muted"><b>View Customers List </b></a></li>
+        </ul>
+        <p class="text-center text-muted"> 2024 GRIP Bank</p>
+      </footer>
+      <!-- footer part end -->
---
+++ include/header.php
@@ -0,0 +1,24 @@
+    <!-- navbar part start -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="./"> <b> GRIP BANK </b> </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php"> Home </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="customer.php"> View Customers </a>
+              </li>
+            </ul>
+
+            <div class="navbar-text text-white" style="position: absolute; top: 0; right: 10px;">
+            Scroll Down 
+            </div>
+          </div>
+        </div>
+      </nav>
+        <!-- navbar part ended -->
